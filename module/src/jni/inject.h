17bbd71c68ecbd8f5906cc24db7b48d9c8e8dff6
mohan585 <71021883+mohan585@users.noreply.github.com>
2026-02-14 21:18:17 +0530

    Add start_injection function and include config.h
    Added start_injection function declaration and included config.h.

--- module/src/jni/inject.h
+++ module/src/jni/inject.h
@@ -1,9 +1,10 @@
 #ifndef ZYGISKFRIDA_INJECT_H
 #define ZYGISKFRIDA_INJECT_H
 
-#include <string>
+#include "config.h"
 
 void inject_lib(std::string const& lib_path, std::string const& logContext);
+void start_injection(target_config const& cfg);
 bool check_and_inject(std::string const& app_name);
 
 #endif  // ZYGISKFRIDA_INJECT_H
